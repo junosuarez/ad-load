f784474850e0626fb72eaa4a05a2d6b0e11f3493
jden <jason@denizac.org>
2013-01-03 16:36:34 -0800

    add timeouts

--- test/test.js
+++ test/test.js
@@ -11,6 +11,10 @@ test('loads a module in another package', function (t) {
 
 	t.plan(3)
 
+	var timeout = setTimeout(function () {
+		t.fail('timeout')
+	}, 8000)
+
 	adLoad = require('../index.js')(require)
 
 	t.equal('function', typeof adLoad, 'fns errywere')
@@ -19,6 +23,7 @@ test('loads a module in another package', function (t) {
 		t.equal('function', typeof y, 'yar')
 		t.equal(6, y(2,3), 'it werks')
 		t.equal(false, true, 'this will fail')
+		clearTimeout(timeout)
 		t.end()
 	}, function () {
 		t.fail('error back should not occur')
