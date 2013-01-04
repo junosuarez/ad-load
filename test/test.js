b3f5c6cb50802e4b4ad086229b3c03f3bf16f64c
jden <jason@denizac.org>
2013-01-03 16:18:02 -0800

    browser test

--- test/test.js
+++ test/test.js
@@ -1,17 +1,18 @@
 var test = require('tape')
 
+test('browsars', function (t) {
+	t.plan(1)
+	t.ok(localStorage in window)
+	t.end()
+})
+
+
 test('loads a module in another package', function (t) {
 
 	t.plan(2)
 
 	adLoad = require('../index.js')(require)
 
-
-	test('browsars', function (t) {
-		t.plan(1)
-		t.ok(localStorage in window)
-		t.end()
-	})
 
 	adLoad('y.js', './y-src').then(function (y) {
 		t.equal('function', typeof y)
