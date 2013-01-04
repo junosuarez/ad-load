242988fdaee1949919fb775c9604ecf7476c96d0
jden <jason@denizac.org>
2013-01-03 16:16:43 -0800

    browser test

--- test/test.js
+++ test/test.js
@@ -6,16 +6,17 @@ test('loads a module in another package', function (t) {
 
 	adLoad = require('../index.js')(require)
 
+
+	test('browsars', function (t) {
+		t.plan(1)
+		t.ok(localStorage in window)
+		t.end()
+	})
+
 	adLoad('y.js', './y-src').then(function (y) {
 		t.equal('function', typeof y)
 		t.equal(6, y(2,3))
 		t.equal(false, true)
 		t.end()
 	})
-})
-
-test('browsars', function (t) {
-	t.plan(1)
-	t.ok(localStorage in window)
-	t.end()
 })
